46612b0186e91251578602de77ce8c910c8bd015
Norlynm <norlynmartinez7@gmail.com>
2024-08-08 17:15:41 -0400

    Version 0.1
    Aqui se crea el registro de usuario, y sus funciones en el programa

--- Gestion_Proyecto_costru/Menu/views.py
+++ Gestion_Proyecto_costru/Menu/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
-# Create your views here.
+def principal(request):
+    return render(request, 'principal.html')
+
+def registro(request):
+    if request.method == 'GET': 
+        return render(request, 'registro.html', {'form': UserCreationForm()})
+    else:
+        # Aqui se compara las contraseñas
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+              # Aqui se crea la clase usuario
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado con éxito')
+            #Aqui estan las condiciones por si el usuario existe y por si no
+            
+            except:
+                return HttpResponse('El usuario ya existe')
+        else:
+            return HttpResponse("Las contraseñas no coinciden")
+
+ 
