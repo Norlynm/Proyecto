b53ba58eaff46e4b2399ec5f1dea7a54d6dca9a1
Norlynm <norlynmartinez7@gmail.com>
2024-08-13 16:13:56 -0400

    Cambios 1
    Cree  los migrates, y arregle algunos urls como tambine agregue algunas funcines en admin

--- Gestion_Proyecto_costru/proyectos/views.py
+++ Gestion_Proyecto_costru/proyectos/views.py
@@ -8,30 +8,31 @@ from .forms import ProyectoForm
 from .models import Proyecto
 from django.views.generic import CreateView,ListView, UpdateView, DeleteView
 
+
 def inicio(request):
-    return render(request, "proyectos/inicio.html", )
+    return render(request, "principal.html", )
 
 class crearproyecto(CreateView):
      model  = Proyecto
      form_class = ProyectoForm
-     template_name = "projects/crearproyecto.html"
+     template_name = "crearproyecto.html"
      success_url = reverse_lazy('projects:mostrarproyecto')
      
    
 class ListarProyecto(ListView):
      model  = Proyecto
-     template_name = "proyectos/proyecto_detalle.html"
+     template_name = "proyecto_detalle.html"
      context_object_name = "proyecto"
 
 class actulizarproyecto(UpdateView):
     model = Proyecto
     form_class = ProyectoForm
-    template_name = 'proyectos/editar_proyecto.html'
+    template_name = 'editar_proyecto.html'
     success_url = reverse_lazy('projects:mostrarproyecto')
     context_object_name = "proyecto"
     
 class eliminarproyecto(DeleteView):
     model = Proyecto
-    template_name = 'proyectos/eliminar_proyecto.html'
+    template_name = 'eliminar_proyecto.html'
     success_url = reverse_lazy('projects:mostrarproyecto')
     context_object_name = 'proyecto'   
