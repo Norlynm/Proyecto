5fa568d4f8836e234b849b920d44f8af34663ffe
Norlyn <norlynmartinez7@gmail.com>
2024-08-23 11:38:30 -0400

    actualizacion de las tareas

--- Gestion_Proyecto_costru/tareas/urls.py
+++ Gestion_Proyecto_costru/tareas/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('creartarea/', views.CrearTarea.as_view(), name='creartarea'),
     path('listartareas/', views.ListarTareas.as_view(), name='listartareas'),
     path('editartarea/<int:pk>', views.ActualizarTarea.as_view(), name='editartarea'),
-    path('tareas_detalle/<int:pk>/', views.DetalleTarea.as_view(), name='tareas_detalle'),
+    path('tareas_detalle/<int:pk>', views.DetalleTarea.as_view(), name='tareas_detalle'),
    
 ]
